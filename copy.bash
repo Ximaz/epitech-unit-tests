609b19fa54d8cb0e5a4a2ac18e78bec2a41c5d39
Ximaz <malo.durand@epitech.eu>
2022-07-12 19:42:35 +0200

    Adding the possibility to change the CPool location.

--- copy.bash
+++ copy.bash
@@ -1,5 +1,12 @@
 #!/bin/bash -e
 
+## This file lets it's user to copy tests from the specified
+## `chapter` from it's desktop. You may have to change the
+## `EPITECH_CPOOL_LOCATION` below to make it matching the
+## path where your EPITECH C pool is located.
+
+EPITECH_CPOOL_LOCATION=~/Desktop/epitech
+
 function usage {
     echo "Usage :"
     echo "${0} <chapter>"
@@ -7,12 +14,16 @@ function usage {
 }
 
 function build_src {
-    Echo ~/Desktop/epitech/$1
+    echo $EPITECH_CPOOL_LOCATION/$1
 }
 
 function check_arguments {
     local SOURCE=$(build_src "${1}")
 
+    if [[ ! -d "${EPITECH_CPOOL_LOCATION}" ]]; then
+        usage "you have to change the EPITECH_CPOOL_LOCATION's value. Current : '${EPITECH_CPOOL_LOCATION}'"
+        exit 1
+    fi
     if [[ "${1}" = "" ]]; then
         usage "no chapter provided."
         exit 1
