be537ad8a11967edf5ebbdc7658f9cf6fa809e74
Ximaz <malo.durand@epitech.eu>
2022-07-12 19:27:30 +0200

    Tracking new tests.

--- B-CPE-100/Day03/test_my_isneg.c
+++ B-CPE-100/Day03/test_my_isneg.c
@@ -1,6 +1,6 @@
 /*
 ** EPITECH UNIT TESTS, 2022
-** Authors : Ximaz
+** Authors : Ximaz, Adam Brutsaert
 ** File description :
 ** Unit tests to test the my_isneg function.
 */
@@ -10,25 +10,31 @@
 
 int my_isneg(int n);
 
-Test(my_isneg, test_one, .init = cr_redirect_stdout)
+Test(my_isneg, negative_value, .init = cr_redirect_stdout)
 {
     my_isneg(-1);
     cr_assert_stdout_eq_str("N");
 }
 
-Test(my_isneg, test_two, .init = cr_redirect_stdout)
-{
-    my_isneg(-2147483648);
-    cr_assert_stdout_eq_str("N");
-}
-
-Test(my_isneg, test_three, .init = cr_redirect_stdout)
+Test(my_isneg, null_value, .init = cr_redirect_stdout)
 {
     my_isneg(0);
     cr_assert_stdout_eq_str("P");
 }
 
-Test(my_isneg, test_four, .init = cr_redirect_stdout)
+Test(my_isneg, positive_value, .init = cr_redirect_stdout)
+{
+    my_isneg(1);
+    cr_assert_stdout_eq_str("P");
+}
+
+Test(my_isneg, minimum_value, .init = cr_redirect_stdout)
+{
+    my_isneg(-2147483648);
+    cr_assert_stdout_eq_str("N");
+}
+
+Test(my_isneg, maximum_value, .init = cr_redirect_stdout)
 {
     my_isneg(2147483647);
     cr_assert_stdout_eq_str("P");
--- B-CPE-100/Day03/test_my_put_nbr.c
+++ B-CPE-100/Day03/test_my_put_nbr.c
@@ -1,6 +1,6 @@
 /*
 ** EPITECH UNIT TESTS, 2022
-** Authors : Ximaz
+** Authors : Ximaz, Adam Brutsaert
 ** File description :
 ** Unit tests to test the my_put_nbr function.
 */
@@ -10,32 +10,32 @@
 
 int my_put_nbr(int n);
 
-Test(my_put_nbr, test_one, .init = cr_redirect_stdout)
+Test(my_put_nbr, negative_value, .init = cr_redirect_stdout)
 {
     my_put_nbr(-1);
     cr_assert_stdout_eq_str("-1");
 }
 
-Test(my_put_nbr, test_two, .init = cr_redirect_stdout)
+Test(my_put_nbr, null_value, .init = cr_redirect_stdout)
 {
     my_put_nbr(0);
     cr_assert_stdout_eq_str("0");
 }
 
-Test(my_put_nbr, test_three, .init = cr_redirect_stdout)
+Test(my_put_nbr, positive_value, .init = cr_redirect_stdout)
+{
+    my_put_nbr(1);
+    cr_assert_stdout_eq_str("1");
+}
+
+Test(my_put_nbr, minimum_value, .init = cr_redirect_stdout)
 {
     my_put_nbr(-2147483648);
     cr_assert_stdout_eq_str("-2147483648");
 }
 
-Test(my_put_nbr, test_four, .init = cr_redirect_stdout)
+Test(my_put_nbr, maximum_value, .init = cr_redirect_stdout)
 {
     my_put_nbr(2147483647);
     cr_assert_stdout_eq_str("2147483647");
-}
-
-Test(my_put_nbr, test_five, .init = cr_redirect_stdout)
-{
-    my_put_nbr(-2147483647);
-    cr_assert_stdout_eq_str("-2147483647");
 }
